d1c7fbf8843a73779f62b25f45db9801201bd168
liu-kn <kn.liubov@gmail.com>
2026-02-27 15:40:27 +0300

    Initial commit

---
+++ learning.py
@@ -0,0 +1,43 @@
+#print("Lyubov Koroleva")
+'''print("Я люблю учить Python!")
+print("Привет, друг!, Как дела?")
+print("Что нового в Python?")
+print('Он сказал: "Привет!"')
+print("It's his favourite language")
+print("Python полезен; он интересен")
+print("Я учу Python каждый день")
+print("Первая строка")
+print("Вторая строка")
+print("Имя: Liubov \nВозраст: 74")
+print("PYTHON ЭТО КРУТО".lower())
+print("привет мир".upper())
+print("Привет! Как дела? Все хорошо.")
+print("Учить Python... это интересно...")
+print("Я люблю Python")
+print('"Я" "люблю" "Python"')
+print("Python #1 @ school")
+print("Привет!\nПривет, как дела?)'''
+
+'''print("Hello", "123", "100")
+
+name = "Liuba"
+age = 30
+print(name, age)
+print("Hello", name)
+print("How is it going?", name)
+print("Bye", name)
+print("123", 100, None, True)
+print (10 // 3) # 3
+print(10 % 3) # 1
+print(10 ** 3)  # 1000
+print("1" + "23") # 123
+
+x = 3
+y = "5"
+int_y = int(y) # "5" -> 5
+str_x = str(x) # 3 -> "3"
+print(x + int_y)'''
+
+message = input()
+print(message)
+
